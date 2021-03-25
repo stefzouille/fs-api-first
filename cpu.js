a04c67da6d0cf3512f672b56350e7ff250a10def
stefzouille <contact.lokcy@gmail.com>
2021-03-25 09:20:33 +0100

    add

--- cpu.js
+++ cpu.js
@@ -3,3 +3,8 @@ var os = require('os');
 console.log(os.cpus());
 console.log(os.totalmem());
 console.log(os.freemem())
+var os = require('os-utils');
+
+os.cpuUsage(function(v){
+    console.log( 'CPU Usage (%): ' + v );
+});
--- firstapi.js
+++ firstapi.js
@@ -5,8 +5,15 @@ const chalk = require('chalk')
 
 const txt = fs.readFileSync('blabla.txt', 'utf-8')
 
-const op = readlineSync.question(chalk.yellow(`type enter to see the txt ?`))
+const op = readlineSync.question(chalk.yellow(`click enter to see the txt and copy in new file ?`))
 
 fs.copyFileSync('blabla.txt', 'text.txt')
+fs.copyFileSync('blabla.txt', 'text.md')
 
 console.log(chalk.red(txt, op))
+
+const startUsage = process.cpuUsage();
+const now = Date.now();
+while (Date.now() - now < 500);
+
+console.log(process.cpuUsage(startUsage));
