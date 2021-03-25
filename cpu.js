d803c4b9f3afbd3ec2cf40fb9a5f4d4f3e3f7ba6
stefzouille <contact.lokcy@gmail.com>
2021-03-25 09:24:36 +0100

    d

--- cpu.js
+++ cpu.js
@@ -1,10 +1,8 @@
-var os = require('os');
+const os = require('os');
 
 console.log(os.cpus());
 console.log(os.totalmem());
 console.log(os.freemem())
-var os = require('os-utils');
 
-os.cpuUsage(function(v){
-    console.log( 'CPU Usage (%): ' + v );
-});
+
+
--- firstapi.js
+++ firstapi.js
@@ -1,16 +1,17 @@
 const fs = require('fs')
 const readlineSync = require('readline-sync')
 const chalk = require('chalk')
-
-
 const txt = fs.readFileSync('blabla.txt', 'utf-8')
 
+
 const op = readlineSync.question(chalk.yellow(`click enter to see the txt and copy in new file ?`))
+
 
 fs.copyFileSync('blabla.txt', 'text.txt')
 fs.copyFileSync('blabla.txt', 'text.md')
 
 console.log(chalk.red(txt, op))
+
 
 const startUsage = process.cpuUsage();
 const now = Date.now();
